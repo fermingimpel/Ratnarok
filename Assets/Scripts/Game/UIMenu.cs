0ee946ed258940937c32da12e47700230b1e0e28
fermingimpel <fermingimpel@gmail.com>
2020-09-29 12:02:41 -0300

    Añadido placeholder de config en el menu Close #333 completed

---
+++ Assets/Scripts/Game/UIMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenu : MonoBehaviour {
+   [SerializeField] GameObject UIConfig;
+    bool configDisplayed = false;
+
+   void Start() {
+
+    }
+    void Update() {
+
+    }
+
+    public void ClickedOptions() {
+        configDisplayed = !configDisplayed;
+        if(configDisplayed) {
+            UIConfig.SetActive(true);
+            return;
+        }
+
+        UIConfig.SetActive(false);
+        return;
+    }
+
+}
