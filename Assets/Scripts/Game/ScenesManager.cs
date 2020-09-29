dadecd1278063f96f8963ad73a0819d1c213e7d9
fermingimpel <fermingimpel@gmail.com>
2020-09-29 19:17:49 -0300

    Añadida pantalla de carga Close #338 completed

--- Assets/Scripts/Game/ScenesManager.cs
+++ Assets/Scripts/Game/ScenesManager.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ScenesManager : MonoBehaviour {
+    [SerializeField] GameObject loadScreen;
+
     public void LoadScene(string stl) {
         Time.timeScale = 1.0f;
-        SceneManager.LoadScene(stl);
+        StartCoroutine(Load(stl));
     }
+
+    IEnumerator Load(string stl) {
+        loadScreen.gameObject.SetActive(true);
+
+        SceneManager.LoadScene(stl);
+        while (!stl.Equals(SceneManager.GetActiveScene().name))
+            yield return null;
+
+        loadScreen.gameObject.SetActive(false);
+        StopCoroutine(Load(name));
+        yield return null;
+    }
+
     public void CloseGame() {
         Application.Quit();
     }
--- Assets/Scripts/Gameplay/UI/UIButtonsGameplay.cs
+++ Assets/Scripts/Gameplay/UI/UIButtonsGameplay.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class UIButtonsGameplay : MonoBehaviour {
     [SerializeField] GameObject pauseMenu;
+
     public void ClickedPauseButton() {
         pauseMenu.SetActive(true);
         Time.timeScale = 0.0f;
