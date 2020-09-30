3b6f3defaa6113202601cad4d542bcf67f78ac16
fermingimpel <fermingimpel@gmail.com>
2020-09-30 10:14:32 -0300

    Cambiado el Ratary

--- Assets/Scripts/Gameplay/UI/UIButtonsGameplay.cs
+++ Assets/Scripts/Gameplay/UI/UIButtonsGameplay.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class UIButtonsGameplay : MonoBehaviour {
     [SerializeField] GameObject pauseMenu;
-
+    [SerializeField] GameObject rataryMenu;
     public void ClickedPauseButton() {
         pauseMenu.SetActive(true);
         Time.timeScale = 0.0f;
     }
     public void ClickedResumeButton() {
         pauseMenu.SetActive(false);
+        rataryMenu.SetActive(false);
         Time.timeScale = 1.0f;
     }
+
+    public void ClickedRatary() {
+        rataryMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
 }
