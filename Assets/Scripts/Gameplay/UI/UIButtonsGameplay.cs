3e7a7cc1d8a508a662df5588abc59fe73a256fdc
fermingimpel <fermingimpel@gmail.com>
2020-11-03 23:10:17 -0300

    Cambiada pantalla de Config en gameplay

--- Assets/Scripts/Gameplay/UI/UIButtonsGameplay.cs
+++ Assets/Scripts/Gameplay/UI/UIButtonsGameplay.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class UIButtonsGameplay : MonoBehaviour {
     [SerializeField] GameObject pauseMenu;
     [SerializeField] GameObject rataryMenu;
+    [SerializeField] GameObject configMenu;
+    [SerializeField] GameObject[] configOpen;
+    bool configDisplayed=true;
+
     public void ClickedPauseButton() {
         pauseMenu.SetActive(true);
         Time.timeScale = 0.0f;
@@ -20,4 +24,26 @@ public class UIButtonsGameplay : MonoBehaviour {
         Time.timeScale = 0f;
     }
 
+    public void ClickedOptions() {
+        configDisplayed = !configDisplayed;
+        if (!configDisplayed) {
+            pauseMenu.SetActive(false);
+            configMenu.SetActive(true);
+            return;
+        }
+
+        pauseMenu.SetActive(true);
+        configMenu.SetActive(false);
+        return;
+    }
+
+    public void ClickedToggleScreenType() {
+        for (int i = 0; i < configOpen.Length; i++)
+            if (configOpen[i] != null) {
+                if (configOpen[i].activeSelf)
+                    configOpen[i].SetActive(false);
+                else
+                    configOpen[i].SetActive(true);
+            }
+    }
 }
